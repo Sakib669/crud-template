5c33414ff8f657d82d7184da38f0393a24dfb70a
Shafiqul Islam Sakib <stewieboyyy@gmail.com>
2026-04-10 23:57:59 +0600

    added social auth in neon auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Home, Sprout } from "lucide-react";
+import { Home, LogIn, LogInIcon, Sprout } from "lucide-react";
 import ModeToggle from "./ModeToggle";
 
 interface Props {}
 
 const Navbar = ({}: Props) => {
-
+  
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop:blur supports-backdrop-filter:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
@@ -46,6 +46,18 @@ const Navbar = ({}: Props) => {
             </Button>
 
             <ModeToggle />
+
+            {/* sign in button */}
+            <Button
+              variant={"ghost"}
+              className="flex items-center gap-2"
+              asChild
+            >
+              <Link href={"/auth/sign-in"}>
+                <LogInIcon className="w-4 h-4" />
+                <span className="hidden lg:inline">Sign In</span>
+              </Link>
+            </Button>
           </div>
         </div>
       </div>
